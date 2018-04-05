615492c8dded38904e61a29f05db241529481a88
usman47 <usmanaura47@gmail.com>
2018-04-05 16:59:49 +0500

    Updated

--- _includes/js/custom.js
+++ _includes/js/custom.js
@@ -7,12 +7,40 @@ $(document).ready(function() {
 	$('#open-menu').click(function() {
 		$('#m-nav').toggleClass("w3-show");
 	});
-});
 
-$(document).ready(function() {  
 	$("html").niceScroll({
 		cursorwidth: "8px",
 		cursorminheight: "140",
 		horizrailenabled: false
+	});
+	
+	$('a[href*="#"]')
+	 
+	  .not('[href="#"]')
+	  .not('[href="#0"]')
+	  .click(function(event) {
+		if (
+		  location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+		  && 
+		  location.hostname == this.hostname
+		) {
+		  var target = $(this.hash);
+		  target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+		  if (target.length) {
+			event.preventDefault();
+			$('html, body').animate({
+			  scrollTop: target.offset().top
+			}, 1000, function() {
+			  var $target = $(target);
+			  $target.focus();
+			  if ($target.is(":focus")) {
+				return false;
+			  } else {
+				$target.attr('tabindex','-1');
+				$target.focus();
+			  };
+			});
+		  }
+		}
 	});
 });
